45ecdd7b30baa7ff06a579c2de95a4abc172c126
Kachit <antoxachaos@gmail.com>
2015-02-06 09:12:45 +0300

    refactor this

---
+++ src/Kachit/Phalcon/Mvc/Model/Filter/AbstractFilter.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * AbstractConfig
+ *
+ * @author Kachit
+ */
+namespace Kachit\Phalcon\Mvc\Model\Filter;
+
+abstract class AbstractFilter {
+
+    /**
+     * @var int
+     */
+    protected $limit;
+
+    /**
+     * @var int
+     */
+    protected $offset;
+
+    /**
+     * @var mixed
+     */
+    protected $groupBy;
+
+    /**
+     * Get Limit
+     *
+     * @return mixed
+     */
+    public function getLimit() {
+        return $this->limit;
+    }
+
+    /**
+     * Set Limit
+     *
+     * @param mixed $limit
+     * @return $this;
+     */
+    public function setLimit($limit) {
+        $this->limit = $limit;
+        return $this;
+    }
+
+    /**
+     * Get Offset
+     *
+     * @return mixed
+     */
+    public function getOffset() {
+        return $this->offset;
+    }
+
+    /**
+     * Set Offset
+     *
+     * @param mixed $offset
+     * @return $this;
+     */
+    public function setOffset($offset) {
+        $this->offset = $offset;
+        return $this;
+    }
+}
---
+++ src/Kachit/Phalcon/Mvc/Model/Repository/AbstractRepository.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * AbstractRepository
+ *
+ * @author Kachit
+ */
+namespace Kachit\Phalcon\Mvc\Model\Repository;
+
+use Kachit\Phalcon\Mvc\Model\Filter\AbstractFilter;
+use Phalcon\DI\Injectable;
+use Phalcon\Mvc\Model\Manager;
+use Phalcon\Mvc\Model;
+use Phalcon\Mvc\Model\Resultset\Simple as Resultset;
+use Phalcon\Mvc\Model\Query\Builder;
+
+abstract class AbstractRepository extends Injectable {
+
+    /**
+     * getModelName
+     *
+     * @return string
+     */
+    abstract public function getModelName();
+
+    /**
+     * @param string $alias
+     *
+     * @return Builder
+     */
+    public function createQuery($alias = null) {
+        return $this->getModelsManager()->createBuilder($alias)
+            ->addFrom($this->getModelName(), $alias);
+    }
+
+    public function findByPk($id) {
+        return $this->createQuery()->where('id = ' . $id)->getQuery()->getSingleResult();
+    }
+
+    /**
+     * findFirst
+     *
+     * @param AbstractFilter $config
+     * @return Model
+     */
+    public function findFirst(AbstractFilter $config) {
+        $query = $this->createQueryByConfig($config);
+        return $query->getQuery()->getSingleResult();
+    }
+
+    /**
+     * findAll
+     *
+     * @param AbstractFilter $config
+     * @return Resultset
+     */
+    public function findAll(AbstractFilter $config) {
+        $query = $this->createQueryByConfig($config);
+        if ($config->getLimit()) {
+            $query->limit($config->getLimit());
+        }
+        if ($config->getOffset()) {
+            $query->offset($config->getOffset());
+        }
+        return $query->getQuery()->execute();
+    }
+
+    /**
+     * createQueryByConfig
+     *
+     * @param AbstractFilter $config
+     * @return Builder
+     */
+    abstract protected function createQueryByConfig(AbstractFilter $config);
+
+    /**
+     * getModelEntity
+     *
+     * @return Model
+     */
+    public function getModelEntity() {
+        $entity = $this->getModelName();
+        return new $entity;
+    }
+
+    /**
+     * getModelsManager
+     *
+     * @return Manager
+     */
+    protected function getModelsManager() {
+        return $this->getDi()->get('modelsManager');
+    }
+}
