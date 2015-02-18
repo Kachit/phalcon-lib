a3c4b9ce870de9104ed83e042b51925c2d3286c5
Antoxa <kornilov@realweb.ru>
2015-02-18 12:51:03 +0300

    Hmvc fixes

--- src/Kachit/Phalcon/Mvc/Controller.php
+++ src/Kachit/Phalcon/Mvc/Controller.php
@@ -8,13 +8,39 @@
 namespace Kachit\Phalcon\Mvc;
 
 use Phalcon\Mvc\Controller as PhalconController;
+use Phalcon\Mvc\Dispatcher;
 
 use Kachit\Phalcon\Http\Response\StatusCode;
 
 class Controller extends PhalconController {
 
     /**
-     * @param $code
+     * @var bool
+     */
+    protected $isHmvcResponse = false;
+
+    /**
+     * After execute route event
+     *
+     * @param Dispatcher $dispatcher
+     */
+    public function afterExecuteRoute(Dispatcher $dispatcher) {
+        if ($this->isHmvcResponse) {
+            $this->createHmvcResponse($dispatcher);
+        }
+    }
+
+    /**
+     * Not found Action
+     */
+    public function notFoundAction() {
+        $this->setStatusNotFound();
+    }
+
+    /**
+     * Set response status code
+     *
+     * @param string $code
      * @param string $message
      */
     protected function setStatusCode($code, $message = null) {
@@ -22,9 +48,19 @@ class Controller extends PhalconController {
     }
 
     /**
-     *
+     * Set status code Not Found
      */
     protected function setStatusNotFound() {
         $this->setStatusCode(StatusCode::NOT_FOUND);
+    }
+
+    /**
+     * Create hmvc response
+     *
+     * @param Dispatcher $dispatcher
+     */
+    protected function createHmvcResponse(Dispatcher $dispatcher) {
+        $this->view->render($dispatcher->getControllerName(), $dispatcher->getActionName())->getContent();
+        $this->response->setContent($this->view->getContent());
     }
 } 
