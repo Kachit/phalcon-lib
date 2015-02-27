c83d7a0fc51da5a5d17c1719eae0e6dc06067f15
Antoxa <kornilov@realweb.ru>
2015-02-27 18:13:48 +0300

    add examples config

---
+++ examples/config/config.php
@@ -0,0 +1,75 @@
+<?php
+return [
+    //Application modules list
+    'modules' => [
+        'foo' => [
+            'className' => 'Module\Foo\Module',
+            'path' => 'foo/Module.php',
+            'routes' => 'foo/config/routes.php'
+        ],
+    ],
+
+    //DI services
+    'services' => [
+        'database',
+        'session',
+        'volt',
+        'router',
+        'loader',
+    ],
+
+    //Concrete service settings
+    'session' => [
+        'adapter'  => 'Files',
+    ],
+
+    'database' => [
+        'adapter'  => 'Mysql',
+        'host'     => 'localhost',
+        'username' => 'user',
+        'password' => 'password',
+        'dbname'   => 'database',
+    ],
+
+    //Application settings
+    'application' => [
+        'debug' => false,
+        'defaultController' => 'index',
+        'defaultAction' => 'index',
+        'defaultModule' => 'foo',
+        'defaultNamespace' => 'Module\Foo\Controllers',
+    ],
+
+    'router' => [
+        'removeExtraSlashes' => true,
+        'enableDefaultRoutes' => true,
+    ],
+
+    //Additional libraries load path
+    'loader' => [
+        'namespaces' => [
+            'Common\Models' => 'models/',
+            'Common\Services' => 'services/',
+        ]
+    ],
+
+    'cache' => [
+        'frontend' => [
+            'adapter' => 'Data',
+        ],
+        'backend' => [
+            'adapter' => 'File',
+            'cacheDir' => '',
+        ],
+    ],
+
+    'volt' => [
+        'compiledPath' => 'volt/',
+        'compiledExtension' => '.compiled',
+        'compileAlways' => true,
+    ],
+
+    'environment' => [
+        'error_reporting' => -1,
+    ],
+];
