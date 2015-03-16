80753d1bdbaea59597e091ac80d1822db8ee2596
Kachit <antoxachaos@gmail.com>
2015-03-16 23:35:11 +0300

    add soft delete for entity

--- src/Kachit/Phalcon/Mvc/Model/Entity/AbstractEntity.php
+++ src/Kachit/Phalcon/Mvc/Model/Entity/AbstractEntity.php
@@ -8,8 +8,17 @@
 namespace Kachit\Phalcon\Mvc\Model\Entity;
 
 use Phalcon\Mvc\Model;
+use Phalcon\Mvc\Model\Behavior\SoftDelete;
 
 abstract class AbstractEntity extends Model {
+
+    const SOFT_DELETE_FIELD = 'deleted';
+    const SOFT_DELETE_VALUE = 1;
+
+    /**
+     * @var bool use soft delete flag
+     */
+    protected $useSoftDelete = false;
 
     /**
      * @return array
@@ -20,5 +29,23 @@ abstract class AbstractEntity extends Model {
             $messages[$messageObject->getField()][$messageObject->getType()] = $messageObject->getMessage();
         }
         return $messages;
+    }
+
+    /**
+     * Initialize
+     */
+    public function initialize() {
+        if ($this->useSoftDelete) {
+            $this->addBehavior($this->getSoftDeleteBehavior());
+        }
+    }
+
+    /**
+     * Get soft delete behavior
+     *
+     * @return SoftDelete
+     */
+    protected function getSoftDeleteBehavior() {
+        return new SoftDelete(['field' => self::SOFT_DELETE_FIELD, 'value' => self::SOFT_DELETE_VALUE]);
     }
 }
