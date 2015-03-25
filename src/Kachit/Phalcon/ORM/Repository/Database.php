53448151b6b6b05108c1f41e4cfdb76e8020e846
Kachit <antoxachaos@gmail.com>
2015-03-26 00:45:20 +0300

    add orm

---
+++ src/Kachit/Phalcon/ORM/Repository/Database.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Database
+ *
+ * @author Kachit
+ * @package Kachit\Phalcon\ORM\Repository
+ */
+namespace Kachit\Phalcon\ORM\Repository;
+
+abstract class Database implements RepositoryInterface {
+
+}
---
+++ src/Kachit/Phalcon/ORM/Repository/Exception.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Exception
+ *
+ * @author Kachit
+ */
+namespace Kachit\Phalcon\ORM\Repository;
+
+class Exception extends \Exception {
+
+}
